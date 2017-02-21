2c6a5c8e6d129f1df9d2e4cf8e1e236a5ddb492b
KatteKurach <kattekurach@gmail.com>
2017-02-22 01:01:23 +0300

    Add new url "/archive".

--- python_blog/urls.py
+++ python_blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^log/$', views.log, name='log'),
     url(r'^sign/$', views.sign, name='sign'),
     url(r'^blog/$', views.blog, name='blog'),
+    url(r'^archive/$', views.archive, name='archive'),
     url(r'$', views.home, name='home'),
 ]
