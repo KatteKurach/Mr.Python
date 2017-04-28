d4114b2cb479055a2329526dc39c623fbdfd352d
KatteKurach <kattekurach@gmail.com>
2017-04-28 08:17:48 +0300

    update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -78,8 +78,25 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+import os
+import psycopg2
+import urlparse
 
-DATABASES = {
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+'''DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "myproject",
@@ -88,7 +105,7 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": "",
     }
-}
+}'''
 
 
 
